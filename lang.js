bb5b6952a7ad36f22e97a8dd9eb31fc2f249d9d2
Vasiliy Star <vasilij.starovojtov@mail.ru>
2024-03-21 21:07:23 +0300

    add: Eng.Rus select

---
+++ lang.js
@@ -0,0 +1,120 @@
+const langArr = {
+    "Contacts":{
+        "ru": "Контакты",
+        "en" : "Contacts",
+    },
+    "Summary":{
+        "ru": "О себе",
+        "en" : "Summary",
+    },
+    "Skills":{
+        "ru": "Способности",
+        "en" : "Skills",
+    },
+    "Projects":{
+        "ru": "Проекты",
+        "en" : "Projects",
+    },
+    "Education":{
+        "ru": "Образование",
+        "en" : "Education",
+    },
+    "Language":{
+        "ru": "Языки",
+        "en" : "Language",
+    },
+    "Name":{
+        "ru": "Василий Старовойтов",
+        "en" : "Vasiliy Starovoytov",
+    },
+    "Prof":{
+        "ru": "Младший Фрогэнд Разработчик",
+        "en" : "Junior Front-end Developer",
+    },
+    "CountryBY":{
+        "ru": "Пинск,Беларусь",
+        "en" : "Pinsk, Belarusr",
+    },
+    "CountryRU":{
+        "ru": "Пенза,Россия",
+        "en" : "Penza, Russia",
+    },
+    "Mobile":{
+        "ru": "Телефон",
+        "en" : "Mobile",
+    },
+    "Email":{
+        "ru": "Почта: vasiliy.starovoytov.96@mail.ru",
+        "en" : "Email: vasiliy.starovoytov.96@mail.ru",
+    },
+    "Github":{
+        "ru": "ГитХаб: https://github.com/Vasyaruberoid",
+        "en" : "Github: https://github.com/Vasyaruberoid",
+    },
+    "Telegram":{
+        "ru": "Телеграм: @Vasiliy_starovojtov",
+        "en" : "Telegram: @Vasiliy_starovojtov",
+    },
+    
+    "Discord":{
+        "ru": "Дискорд: vasiliystarovoytov",
+        "en" : "Discord: vasiliystarovoytov",
+    },
+    
+    "Code wars":{
+        "ru": "КодВарс: https://www.codewars.com/users/Vasyaruberoid",
+        "en" : "CodeWars: https://www.codewars.com/users/Vasyaruberoid",
+    },
+    "AboutMe":{
+        "ru": "Развитие - цель моей жизни",
+        "en" : "My meaning in life is development.",
+    },
+    "Prof":{
+        "ru": "Веб-разработка",
+        "en" : "Web-development:",
+    },
+    "Freaemwork":{
+        "ru": "Фреймворк: React",
+        "en" : "Frameworks and libraies: React",
+    },
+    "Vers":{
+        "ru": "Контроль версий: Git,github",
+        "en" : "Version control: Git,github",
+    },
+    "Data":{
+        "ru": "Дата-базы: Firebase,MongoDB,Wordpress,My SQL",
+        "en" : "Databases: Firebase,MongoDB,Wordpress,My SQL",
+    },
+    "Graphics":{
+        "ru": "Графические редакторы:Figma",
+        "en" : "Graphics:Figma",
+    },
+    "Course":{
+        "ru": "Курсы",
+        "en" : "Course",
+    },
+    "Proj":{
+        "ru": "Резюме Магазин Велосипед Калькуляьор",
+        "en" : "Resume BikeShop Calculator",
+    },
+    "Educ":{
+        "ru": "Белорусская государственная сельскохозяйственная Академия",
+        "en" : "Belarusian State Agricultural Academy(engineer)",
+    },
+    "Rus":{
+        "ru": "Русский - А2",
+        "en" : "Russian - A2",
+    },
+      "By":{
+        "ru": "Белоруский - А2",
+        "en" : "Belorussian",
+    },
+      "Eng":{
+        "ru": "Английский - А2",
+        "en" : "English - A2 intermediate",
+    },
+      "Pl":{
+        "ru": "Польский - А2",
+        "en" : "Polish - A2",
+    },
+}
